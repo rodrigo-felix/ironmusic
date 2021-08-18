4b6f2238350c03ef91c5b251c977feb957a38e00
rodrigo-felix <rodrigofelix.leite@gmail.com>
2021-08-17 21:03:15 -0300

    Search card done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+
 const Home = () => {
     return (
         <div className="container">
             <h1 className="text-center" style={ {paddingTop: "30%" }}>
                 Home
             </h1>
+            <div className="card-search card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Busca</h5>
+                    <form>
+                        <input type="email" class="form-control" id="search-artist" placeholder="Busque pelo artista :)"></input><br></br>
+                        <button type="submit" class="btn btn-primary">Buscar e ouvir</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
