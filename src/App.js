1271cf8c93bad82702e1d317c37cdd5cac13f1c5
Evaniomne <edmoura@leroymerlin.com.br>
2021-08-17 21:08:36 -0300

    Footer concluido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
-import './App.css';
+
+import './components/Footer/Footer.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
 
     <div>
       <h1>IronMusic</h1>
+      <Footer    
+      
+      />
     </div>
 
   );
